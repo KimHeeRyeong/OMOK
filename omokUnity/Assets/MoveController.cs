8b82047724360770fbb6063eb8fc4c2d5cac0f11
KimHeeRyeong <khr95039@gmail.com>
2019-08-29 10:22:38 +0900

    Add : previous omok project

---
+++ omokUnity/Assets/MoveController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MoveController : MonoBehaviour
+{
+    public Tilemap tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
