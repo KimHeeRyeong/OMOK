e81dee843ea3b912837e39c764015beddd6f326d
KimHeeRyeong <khr95039@gmail.com>
2019-09-03 12:57:33 +0900

    Feature:make server for more people
    close #20

--- Server/Server/Echo.cs
+++ Server/Server/Echo.cs
@@ -17,38 +17,45 @@ namespace Server
 
         OmokState stage;
         PosState myState;
-        protected override void OnOpen() {//this에 해당하는 유저 접속
-            if (users.Count > 2)
-            {
-                //내보내기
-            }
-            else
-            {
-                users.Add(this);
-                if (users.Count == 2)
+        Echo oppenent = null;
+        protected override void OnOpen()
+        {
+            lock (users) {
+                int cnt = users.Count;
+                for (int i = 0; i < cnt; i++)
                 {
-                    stage = new OmokState();
-                    stage.ResetPan();
-                    for (int i = 0; i < 2; i++)
+                    if (users[i].oppenent == null)
                     {
+                        users[i].oppenent = this;
+                        oppenent = users[i];
+
+                        //make stage
+                        stage = new OmokState();
+                        stage.ResetPan();
+                        oppenent.stage = stage;
+
+                        //setState
+                        oppenent.myState = PosState.Black;
+                        myState = PosState.White;
+                        
+                        //setting start_op
                         Start start = new Start();
-                        if (i == 0)
-                        {
-                            users[i].stage = stage;
-                            users[i].myState = PosState.Black;
-                        }
-                        else
-                        {
-                            users[i].myState = PosState.White;
-                        }
-                        start.state = users[i].myState;
+                        start.state = oppenent.myState;
                         string str = JsonConvert.SerializeObject(start);
-                        users[i].Send(str);
+                        oppenent.Send(str);
+
+                        //setting start_this
+                        start.state = myState;
+                        str = JsonConvert.SerializeObject(start);
+                        Send(str);
+                        break;
                     }
                 }
-                   
+                users.Add(this);
             }
+
         }
+        
         protected override void OnMessage(MessageEventArgs e) {//유저로부터 메시지를 받은 경우
             lock (users)
             {
@@ -59,13 +66,7 @@ namespace Server
                         Play play= JsonConvert.DeserializeObject<Play>(e.Data);
                         if(!stage.SetStone(myState, play.m, play.n))
                         {
-                            for (int i = 0; i < 2; i++)
-                            {
-                                if (users[i] != this)
-                                {
-                                    users[i].Send(e.Data);
-                                }
-                            }
+                           oppenent.Send(e.Data);
                         }
                         else
                         {
@@ -74,10 +75,8 @@ namespace Server
                             end.m = play.m;
                             end.n = play.n;
                             string str = JsonConvert.SerializeObject(end);
-                            for(int i = 0; i < 2; i++)
-                            {
-                                users[i].Send(str);
-                            }
+                            Send(str);
+                            oppenent.Send(str);
                         }
                         break;
                 }
